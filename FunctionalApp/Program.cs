3bee8c1f185bb7caa08fc6fc567a57bc23c766bd
Nick <nickmartin1ee7@gmail.com>
2021-08-08 19:22:22 -0400

    Initial commit

---
+++ FunctionalApp/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+using System;
+using System.Linq;
+
+var input = 20;
+
+if (input.ValidateAll(x => x.IsPositive(),
+                   x => x.LessThan(60),
+                   x => x.GreaterThanOrEqualTo(0)))
+{
+    // ...
+}
+*/
+
+using System;
+using System.Linq;
+using Functionals;
+using static FunctionalApp.Application;
+
+namespace FunctionalApp
+{
+    public class Program
+    {
+        public static void Main(string[] args) => args
+            .Ignore()
+            
+            .With("Enter ", "something: ")
+            .Do(x =>
+                    Print(x.First()), x =>
+                    Print(x.Last()))
+            
+            .With("You entered: ")
+            .With(PromptUser)
+            .Do(PrintWithLine)
+            
+            .With(_ => "End of program").Do(Print)
+            .Do(Wait);
+    }
+
+    public static class Application
+    {
+        public static readonly Action<string> Print = Console.Write;
+        public static readonly Action<string> PrintWithLine = Console.WriteLine;
+        
+        public static readonly Action<Nothing> Wait = _ => Console.ReadKey();
+        
+        public static readonly Func<string, string> PromptUser = x => x + Console.ReadLine();
+    }
+}
---
+++ Functionals/Functionality.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Functionals
+{
+    public static class Miscellaneous
+    {
+
+        public static Nothing Ignore<TInput>(this TInput _) =>
+            new Nothing();
+
+    }
+
+    public static class Providers
+    {
+        public static TOutput With<TInput, TOutput>(this TInput @this, Func<TInput, TOutput> f) => f(@this);
+
+        public static TOutput With<Nothing, TOutput>(this Nothing _, TOutput thing) => thing;
+
+        public static IEnumerable<TOutput> With<Nothing, TOutput>(this Nothing _, params TOutput[] things) => things;
+
+    }
+    
+    public static class Executors
+    {
+        public static Nothing Do<TInput>(this TInput @this, Action<TInput> f)
+        {
+            f(@this);
+            return new Nothing();
+        }
+        
+        public static Nothing Do<TInput>(this TInput @this, params Action<TInput>[] fs)
+        {
+            foreach (var f in fs)
+                f(@this);
+            return new Nothing();
+        }
+    }
+    
+    public static class Validations
+    {
+        public static bool ValidateAll<TInput>(this TInput input, params Func<TInput, bool>[] validators) =>
+            validators.All(v => v(input));
+    
+        public static bool ValidateAny<TInput>(this TInput input, params Func<TInput, bool>[] validators) =>
+            validators.Any(v => v(input));
+
+        public static bool IsPositive(this int input) => input >= 0;
+        public static bool LessThan(this int input, int validation) => input < validation;
+        public static bool GreaterThanOrEqualTo(this int input, int validation) => input >= validation;
+    }
+}
